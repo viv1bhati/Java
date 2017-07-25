77cd845150c190d7a5f1e2902d41589586d26670
viv1bhati <viv1bhati@gmail.com>
2017-07-26 01:49:53 +0530

    Add files via upload

---
+++ ArrayAvg.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int []m = new int[3];
+    m[0] = 10;
+    m[1] = 20;
+    m[2] = 30;
+	double d = (m[0]+m[1]+m[2])/3;
+	System.out.println("Average is : "+d);
+	}
+}
